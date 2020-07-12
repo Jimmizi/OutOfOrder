467e5e79c69049dc9a67ef1c29ef595222720cdb
SuperIzzo <hristoz2004@gmail.com>
2020-07-12 11:31:06 +0100

    Add enemy animations and variant prefabs

---
+++ Assets/Script/Grid/EnemyAnimationController.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAnimationController : MonoBehaviour
+{
+    GridEnemy _enemy;
+    GridEnemy enemy
+    {
+        get
+        {
+            if(!_enemy) { _enemy = GetComponent<GridEnemy>(); }
+            return _enemy;
+        }
+    }
+
+    SpriteRenderer _renderer;
+    SpriteRenderer renderer
+    {
+        get
+        {
+            if (!_renderer) { _renderer = GetComponent<SpriteRenderer>(); }
+            return _renderer;
+        }
+    }
+
+    float animationFrame;
+
+    [SerializeField]
+    float animationSpeed;
+
+    [SerializeField]
+    List<Sprite> IdleFrames;
+
+    [SerializeField]
+    List<Sprite> MoveFrames;
+    
+    void Update()
+    {
+        animationFrame += animationSpeed * Time.deltaTime;
+
+        List<Sprite> currentAnim = IdleFrames;
+
+        if (enemy && enemy.IsMoving)
+        {
+            currentAnim = MoveFrames;
+        }
+
+        if (renderer && currentAnim != null && currentAnim.Count > 0)
+        {
+            int frameIndex = ((int)animationFrame) % currentAnim.Count;
+            renderer.sprite = currentAnim[frameIndex];
+        }
+    }
+}
