51daa8d9a7191e94c5fb214a9ca77d5c0406608f
SuperIzzo <hristoz2004@gmail.com>
2020-07-12 12:39:28 +0100

    Add cog animation

---
+++ Assets/Script/SimpleSpriteAnim.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleSpriteAnim : MonoBehaviour
+{
+    SpriteRenderer _renderer;
+    SpriteRenderer renderer
+    {
+        get
+        {
+            if (!_renderer) { _renderer = GetComponent<SpriteRenderer>(); }
+            return _renderer;
+        }
+    }
+
+    float animationFrame;
+
+    [SerializeField]
+    float animationSpeed;
+
+    [SerializeField]
+    List<Sprite> frames;
+
+    void Update()
+    {
+        animationFrame += animationSpeed * Time.deltaTime;
+        if(animationFrame >= frames.Count)
+        {
+            animationFrame -= frames.Count;
+        }
+
+        if (renderer && frames != null && frames.Count > 0)
+        {
+            int frameIndex = ((int)animationFrame) % frames.Count;
+            renderer.sprite = frames[frameIndex];
+        }
+    }
+}
